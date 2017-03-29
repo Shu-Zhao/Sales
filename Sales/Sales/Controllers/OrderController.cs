91b94a5068a90b60e5a528a7c14acfa40bd9f978
Shu-Zhao <zoe5120tw@gmail.com>
2017-03-29 19:51:00 +0800

    20170329

---
+++ Sales/Sales/Controllers/OrderController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sales.Controllers
+{
+    public class OrderController : Controller
+    {
+        /// <summary>
+        /// 訂單管理首頁
+        /// </summary>
+        /// <returns></returns>
+        // GET: Order
+        public ActionResult Index()
+        {
+            //Models.OrderService orderService = new Models.OrderService();
+            //var order = orderService.GetOrderById("111");
+            //ViewBag.CustId = order.CustId;
+            //ViewBag.CustName = order.CustName;
+
+            ViewBag.Desc1 = "我是ViewBag";
+            ViewData["Desc2"] = "我是ViewData";
+            TempData["Desc3"] = "我是TempData";
+
+            return View();
+        }
+
+        public ActionResult Index2(string id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+
+        /// <summary>
+        /// 新增訂單的畫面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult InsertOrder()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 新增訂單的Action
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [HttpPost()]
+        public ActionResult InsertOrder(Models.Order order)
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            orderService.InsertOrder(order);
+            return View("Index");
+        }
+
+        [HttpGet()]
+        public JsonResult TestJson()
+        {
+            var result = new Models.Order();
+            result.CustId = "GSS";
+            result.CustName = "叡揚資訊";
+
+            //var result = new Models.Order(){CustId="GSS",CustName = "叡揚資訊"};
+
+            return this.Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Sales/Sales/Models/OrderService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sales.Models
+{
+    /// <summary>
+    /// 訂單的服務
+    /// </summary>
+    public class OrderService
+    {
+        /// <summary>
+        /// 新增訂單
+        /// </summary>
+        public void InsertOrder(Models.Order order)
+        {
+            
+        }
+
+        /// <summary>
+        /// 刪除訂單By Id
+        /// </summary>
+        public void DeleteOrderById(string orderId)
+        {
+            
+        }
+
+        /// <summary>
+        /// 修改訂單
+        /// </summary>
+        public void UpdateOrder(Models.Order order)
+        {
+            
+        }
+
+        /// <summary>
+        /// 查詢訂單
+        /// </summary>
+        /// <param name="id">訂單ID</param>
+        /// <returns></returns>
+        public Models.Order GetOrderById(string id)
+        {
+            return new Order(); 
+        }
+
+        /// <summary>
+        /// 查詢訂單
+        /// </summary>
+        /// <returns></returns>
+        public List<Models.Order> GetOrders()
+        {
+            return new List<Order>();
+        }
+        
+
+    }
+}
